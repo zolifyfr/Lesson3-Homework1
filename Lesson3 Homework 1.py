2d62af385f239a35675244d2c26964c373616367
zolifyfr <jayjaynix1@gmail.com>
2026-07-04 20:34:21 -0400

    Add files via upload

---
+++ Lesson3 Homework 1.py
@@ -0,0 +1,9 @@
+# this is the string
+text = "CongrAtulAtions"
+
+# print the string
+print("Original String:", text)
+
+# put them in upper case and lower case
+print("Upper Case:", text.upper())
+print("Lower Case:", text.lower())
